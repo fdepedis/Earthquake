2cd11ad13942041e49336eeda18977fecfc4fe4f
Flavio <flavio.depedis@gmail.com>
2021-01-17 11:59:22 +0100

    add EarthquakeApp class

--- app/src/main/java/it/fdepedis/earthquake/EarthquakeApp.java
+++ app/src/main/java/it/fdepedis/earthquake/EarthquakeApp.java
@@ -1,9 +1,6 @@
 package it.fdepedis.earthquake;
 
 import android.app.Application;
-import android.util.Log;
-
-import androidx.annotation.NonNull;
 import timber.log.Timber;
 
 public class EarthquakeApp extends Application {
@@ -12,27 +9,6 @@ public class EarthquakeApp extends Application {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
-        } else {
-            Timber.plant(new CrashReportingTree());
-        }
-    }
-
-    /** A tree which logs important information for crash reporting. */
-    private static class CrashReportingTree extends Timber.Tree {
-        @Override protected void log(int priority, String tag, @NonNull String message, Throwable t) {
-            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
-                return;
-            }
-
-            /*FakeCrashLibrary.log(priority, tag, message);
-
-            if (t != null) {
-                if (priority == Log.ERROR) {
-                    FakeCrashLibrary.logError(t);
-                } else if (priority == Log.WARN) {
-                    FakeCrashLibrary.logWarning(t);
-                }
-            }*/
         }
     }
 }
